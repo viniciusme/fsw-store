11ee41b60cd03c83d8cd9054fdfe7295e7f2bb90
Vinicius Mendes <vinimendesdev@gmail.com>
2023-11-18 13:48:55 -0300

    chore: add s3 domain to next config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fsw-store.s3.sa-east-1.amazonaws.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
