9553749db9b63ad80ec6a4cd8d056e8b03eeb5c2
Vinicius Mendes <vinimendesdev@gmail.com>
2023-11-19 11:09:42 -0300

    feat: added Footer components

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="bg-accent px-8 py-4">
+      <p className="text-[0.625rem] font-normal opacity-75">
+        © 2023 Copyright <span className="font-semibold">FSW Store</span>
+      </p>
+    </div>
+  );
+}
