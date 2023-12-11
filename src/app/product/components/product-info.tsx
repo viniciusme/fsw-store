a0dd07c7e1b67f50624e94d76472b59a2a8e021a
Vinicius Mendes <vinimendesdev@gmail.com>
2023-12-11 20:50:58 -0300

    feat: add DiscountBadge component

--- src/app/product/components/product-info.tsx
+++ src/app/product/components/product-info.tsx
@@ -1,14 +1,9 @@
 "use client";
 
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import DiscountBadge from "@/components/ui/discount-badge";
 import { productWithTotalPrice } from "@/helpers/products";
-import {
-  ArrowDownIcon,
-  ArrowLeftIcon,
-  ArrowRightIcon,
-  TruckIcon,
-} from "lucide-react";
+import { ArrowLeftIcon, ArrowRightIcon, TruckIcon } from "lucide-react";
 import { useState } from "react";
 
 interface ProductInfoProps {
@@ -39,10 +34,7 @@ const ProductInfo = ({
         <h1 className="text-xl font-bold">R$ {totalPrice.toFixed(2)}</h1>
 
         {discountPercentage > 0 && (
-          <Badge className="px-2 py-[2px]">
-            <ArrowDownIcon size={12} />
-            {discountPercentage}% OFF
-          </Badge>
+          <DiscountBadge>{discountPercentage}</DiscountBadge>
         )}
       </div>
 
---
+++ src/components/ui/discount-badge.tsx
@@ -0,0 +1,16 @@
+import { ArrowDownIcon } from "lucide-react";
+import { Badge, BadgeProps } from "./badge";
+import { twMerge } from "tailwind-merge";
+
+const DiscountBadge = ({ children, className, ...props }: BadgeProps) => {
+  return (
+    <>
+      <Badge className={twMerge("px-2 py-[2px]", className)} {...props}>
+        <ArrowDownIcon size={12} />
+        {children}% OFF
+      </Badge>
+    </>
+  );
+};
+
+export default DiscountBadge;
