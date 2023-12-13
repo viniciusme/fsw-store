3ae90078a2c32bc0439e6258a7841212cf79e739
Vinicius Mendes <vinimendesdev@gmail.com>
2023-12-12 21:23:41 -0300

    feat: add CartItem component and improved cart product addition

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import { productWithTotalPrice } from "@/helpers/products";
 import { Product } from "@prisma/client";
 import { ReactNode, createContext, useState } from "react";
 
-interface CartProduct extends Product {
+export interface CartProduct extends productWithTotalPrice {
   quantity: number;
 }
 
@@ -27,6 +28,29 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
   const [products, setProducts] = useState<CartProduct[]>([]);
 
   const addProductToCart = (product: CartProduct) => {
+    // se o produto já estiver no carrinho, apenas aumente a sua quantidade
+    const productIsAlreadyOnCart = products.some(
+      (cartProduct) => cartProduct.id === product.id,
+    );
+
+    if (productIsAlreadyOnCart) {
+      setProducts((prev) =>
+        prev.map((cartProduct) => {
+          if (cartProduct.id === product.id) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + product.quantity,
+            };
+          }
+
+          return cartProduct;
+        }),
+      );
+
+      return;
+    }
+
+    // se não, adicione o produto à lista
     setProducts((prev) => [...prev, product]);
   };
 
