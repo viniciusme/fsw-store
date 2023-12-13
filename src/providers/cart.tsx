8eb14d6e6915e52a66429d7091524cdc0e78e762
Vinicius Mendes <vinimendesdev@gmail.com>
2023-12-12 21:47:33 -0300

    feat: ensure product quantity can be increased from cart

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -15,6 +15,7 @@ interface ICartContext {
   cartTotalDiscount: number;
   addProductToCart: (product: CartProduct) => void;
   decreaseProductQuantity: (productId: string) => void;
+  increaseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -24,6 +25,7 @@ export const CartContext = createContext<ICartContext>({
   cartTotalDiscount: 0,
   addProductToCart: () => {},
   decreaseProductQuantity: () => {},
+  increaseProductQuantity: () => {},
 });
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -58,8 +60,8 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
 
   const decreaseProductQuantity = (productId: string) => {
     // se a quantidade do produto for 1, remova-o do carrinho
-
     // Se a quantidade do produto for maior que 1, diminua a quantidade em 1
+
     setProducts((prev) =>
       prev
         .map((cartProduct) => {
@@ -76,12 +78,30 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
     );
   };
 
+  const increaseProductQuantity = (productId: string) => {
+    // aumente a quantidade do produto em 1
+
+    setProducts((prev) =>
+      prev.map((cartProduct) => {
+        if (cartProduct.id === productId) {
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + 1,
+          };
+        }
+
+        return cartProduct;
+      }),
+    );
+  };
+
   return (
     <CartContext.Provider
       value={{
         products,
         addProductToCart,
         decreaseProductQuantity,
+        increaseProductQuantity,
         cartTotalPrice: 0,
         cartBasePrice: 0,
         cartTotalDiscount: 0,
