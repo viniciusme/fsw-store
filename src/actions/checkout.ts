765f5c104fcb2c580254f479d0c0f4f9a14d8b05
Vinicius Mendes <vinimendesdev@gmail.com>
2023-12-15 16:42:59 -0300

    feat: add stripe checkout to cart

---
+++ src/actions/checkout.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { CartProduct } from "@/providers/cart";
+import Stripe from "stripe";
+
+export const createCheckout = async (products: CartProduct[]) => {
+  // Create a Stripe client.
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2023-10-16",
+  });
+
+  // Create a Checkout Session.
+  const checkout = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    mode: "payment",
+    success_url: `http://localhost:3000`,
+    cancel_url: `http://localhost:3000`,
+
+    line_items: products.map((product) => ({
+      price_data: {
+        currency: "brl",
+        product_data: {
+          name: product.name,
+          description: product.description,
+          images: product.imageUrls,
+        },
+        unit_amount: product.totalPrice * 100,
+      },
+      quantity: product.quantity,
+    })),
+  });
+
+  // Redirect to Checkout.
+
+  return checkout;
+};
--- src/components/ui/Cart.tsx
+++ src/components/ui/Cart.tsx
@@ -7,9 +7,19 @@ import { computeProductTotalPrice } from "@/helpers/products";
 import { Separator } from "./separator";
 import { ScrollArea } from "./scroll-area";
 import { Button } from "./button";
+import { createCheckout } from "@/actions/checkout";
+import { loadStripe } from "@stripe/stripe-js";
 
 const Cart = () => {
   const { products, subTotal, total, totalDiscount } = useContext(CartContext);
+
+  const handleFinishPurchaseClick = async () => {
+    const checkout = await createCheckout(products);
+
+    const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+    stripe?.redirectToCheckout({ sessionId: checkout.id });
+  };
 
   return (
     <div className="flex h-full flex-col gap-5">
@@ -68,7 +78,12 @@ const Cart = () => {
           <p>R$ {total.toFixed(2)}</p>
         </div>
 
-        <Button className="mt-7 font-bold uppercase">Finalizar compra</Button>
+        <Button
+          className="mt-7 font-bold uppercase"
+          onClick={handleFinishPurchaseClick}
+        >
+          Finalizar compra
+        </Button>
       </div>
     </div>
   );
